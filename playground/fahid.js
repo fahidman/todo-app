0a31c062e8a13a64c9a14b1ac07f6f6526373c2e
Fahid M <fahidroid@gmail.com>
2017-01-28 09:43:51 +0530

    Change of fahid.js

--- playground/fahid.js
+++ playground/fahid.js
@@ -1,34 +1,18 @@
-const {MongoClient,ObjectID} = require('mongodb');
-var obj = new ObjectID();
-MongoClient.connect('mongodb://localhost:27017/Fahid',(err,db)=>{
+const {MongoClient,ObjectID} = require('mongodb')
+
+MongoClient.connect("mongodb://localhost:27017/TodoApp",(err,db)=>{
 	if(err)
-	{
-		return console.log("Error: ",err)
-	}
-	console.log("Successfully connected to mongodb");
+		console.log("Error connecting to db: ",err)
+	console.log("mongodb successfully connected");
 
-	// db.collection('Resume').insertOne({
-	// 	_id: obj,
-	// 	name: "Rahul",
-	// 	age : 191,
-	// 	hobbies: "Girls, All rounder, basketball",
-	// 	Islam: false
-
-	// },(err,res)=>{
-	// 	console.log(res.ops)
-	// })
-
-	// db.collection('Resume').find().count().then((count)=>{
-	// 	console.log(`The Resume collection count : ${count}`)
-	// },(err)=>{
-	// 	console.log("unable to fetch todos",err)
-	// });
-
-	db.collection('Resume').find({name: "Rahul"}).count().then((docs)=>{
-		console.log(docs);
-	},(err)=>{
-		console.log(err)
+   // db.collection('Todos').deleteOne({text: "Eat lunch"}).then((res)=>{
+   // 	console.log(res)
+   // },(err)=>{
+   // 	console.log(err)
+   // })
+	db.collection('Todos').findOneAndDelete({_id : new ObjectID('588c12846fadb9f976039718')}).then((res)=>{
+		console.log(res)
 	})
-	// db.collection('Resume').count((err,count))
+	db.collection('Todos').deleteMany({completed:false})
 	db.close();
 })
